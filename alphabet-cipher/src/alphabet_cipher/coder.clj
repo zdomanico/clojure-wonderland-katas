dfdfa6640b11fa024391788f9fbf13caadb13868
Zach Domanico <zdomanico@gmail.com>
2019-07-16 11:20:06 -0700

    decoder works!

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -17,7 +17,7 @@
 )
 
 
-(defn buildgrid []
+(defn buildgridencode []
   (let [l (a-z)
         r (for [x (a-z)] (zipmap (a-z) (row x)))]
     (zipmap l r)  
@@ -25,11 +25,23 @@
 )
 
 (defn encode-letter [c1 c2]
-  (let [grid (buildgrid)]
+  (let [grid (buildgridencode)]
     ((grid c1) c2)
   )
 )
 
+(defn buildgriddecode []
+  (let [l (a-z)
+        r (for [x (a-z)] (zipmap (row x) (a-z) ))]
+    (zipmap l r)  
+  )
+)
+
+(defn decode-letter [c1 c2]
+  (let [grid (buildgriddecode)]
+    ((grid c1) c2)
+  )
+)
 
 (defn encode [keyword message]
   (let [full_key (take (count message) (cycle keyword))]
@@ -45,7 +57,16 @@
 )
 
 (defn decode [keyword message]
-  
+  (let [full_key (take (count message) (cycle keyword))]
+    (loop [[k & key] (clojure.string/upper-case (apply str full_key)) 
+           [m & mes] (clojure.string/upper-case message)
+           secret []]
+      (if (nil? k) 
+        (clojure.string/lower-case (apply str secret))
+        (recur key mes (conj secret (decode-letter k m)))
+      )
+    )
+  )
 )
 
 (defn decipher [cipher message]
